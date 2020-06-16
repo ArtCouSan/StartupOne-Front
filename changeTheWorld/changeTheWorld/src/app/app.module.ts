be9c33424fc31cffb4a38f5dd36deeaf0f20b649
Arthur Coutinho Santos <arthurcoutinho1998@gmail.com>
2020-06-15 22:41:45 -0300

    feat: navbar feito;feat: tooltip feito;feat: sidebar home feito

--- changeTheWorld/changeTheWorld/src/app/app.module.ts
+++ changeTheWorld/changeTheWorld/src/app/app.module.ts
@@ -3,23 +3,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 import { PostEditorComponent } from './posts/post-editor/post-editor.component';
 import { PostsListComponent } from './posts/post-list/post-list.component';
 import { PostDetailsComponent } from './posts/post-details/post-details.component';
-
+import { SidebarComponent } from './posts/post-list/sidebar/sidebar.component';
+import { SidebarRightComponent } from './posts/post-list/sidebar-right/sidebar-right.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { TooltipComponent } from './shared/components/tooltip/tooltip.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent,
     PostsListComponent,
-    SidebarRightComponent,
     PostEditorComponent,
-    PostDetailsComponent
+    PostDetailsComponent,
+    SidebarComponent,
+    SidebarRightComponent,
+    TooltipComponent
   ],
   imports: [
     BrowserModule,
